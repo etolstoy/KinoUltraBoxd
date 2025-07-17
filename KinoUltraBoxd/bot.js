a4144bf455af800bfa1c1b73c428aaa43a9017b5
Egor Tolstoy <igrekde@gmail.com>
2025-07-17 12:32:42 +0100

    Initial commit

---
+++ KinoUltraBoxd/bot.js
@@ -0,0 +1,12 @@
+const { Telegraf } = require('telegraf');
+require('dotenv').config();
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.start((ctx) => ctx.reply('Hello'));
+
+bot.launch();
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM')); 
